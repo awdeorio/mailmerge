5e71049908100191616640f241a19b5e278a324c
Andrew DeOrio <awdeorio@gmail.com>
2016-06-17 08:47:08 -0400

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 setup(
     name="mailmerge",
     description = "A simple, command line mail merge tool",
-    version="0.12",
+    version="1.3",
     author="Andrew DeOrio",
     author_email="awdeorio@umich.edu",
     url="https://github.com/awdeorio/mailmerge/",
