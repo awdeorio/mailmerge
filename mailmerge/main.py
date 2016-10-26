2faf01c50cf050af17804f09c585a67b06c0c45c
Andrew DeOrio <awdeorio@gmail.com>
2016-10-25 20:57:32 -0400

    Added configuration file for SMTP server

--- mailmerge/main.py
+++ mailmerge/main.py
@@ -8,6 +8,7 @@ import os
 import sys
 import smtplib
 import email.parser
+import ConfigParser #FIXME: not python3 compatible
 from builtins import input  # for Python 2 and 3 compatibility
 import getpass
 import csv
@@ -16,26 +17,37 @@ import click
 
 
 # Configuration
-SMTP_SERVER_HOST = "smtp.mail.umich.edu"
-SMTP_SERVER_PORT = 465
 TEMPLATE_FILENAME_DEFAULT = "mailmerge_template.txt"
 DATABASE_FILENAME_DEFAULT = "mailmerge_database.csv"
+CONFIG_FILENAME_DEFAULT = "mailmerge_config.conf"
 
 
-def sendmail(text):
+def sendmail(text, config_filename):
     """Send email message using Python SMTP library"""
 
-    # Prompt for username and password
-    if not hasattr(sendmail, "username"):
-        print("Credentials for {}".format(SMTP_SERVER_HOST))
-        sendmail.username = input("username: ")
-        sendmail.password = getpass.getpass("password: ")
+    # Read config file from disk to get SMTP server host, port, username
+    # FIXME: move config stuff out of this function?
+    if not hasattr(sendmail, "host"):
+        config = ConfigParser.RawConfigParser()
+        config.read(config_filename)
+        sendmail.host = config.get("smtp_server", "host")
+        sendmail.port = config.get("smtp_server", "port")
+        sendmail.username = config.get("smtp_server", "username")
+        print(">>> Read SMTP server configuration from {}".format(config_filename))
+        print(">>>   host = {}".format(sendmail.host))
+        print(">>>   port = {}".format(sendmail.port))
+        print(">>>   username = {}".format(sendmail.username))
+
+    # Prompt for password
+    if not hasattr(sendmail, "password"):
+        sendmail.password = getpass.getpass(">>> password for {} on {}: ".format(
+            sendmail.username, sendmail.host))
 
     # Parse message headers
     message = email.parser.Parser().parsestr(text)
 
     # Send message
-    smtp = smtplib.SMTP_SSL(SMTP_SERVER_HOST, SMTP_SERVER_PORT)
+    smtp = smtplib.SMTP_SSL(sendmail.host, sendmail.port)
     smtp.login(sendmail.username, sendmail.password)
     smtp.sendmail(
         message["from"],
@@ -44,7 +56,9 @@ def sendmail(text):
         )
     smtp.close()
 
-def create_sample_input_files(template_filename, database_filename):
+def create_sample_input_files(template_filename,
+                              database_filename,
+                              config_filename):
     """Create sample template email and database"""
     print("Creating sample template email {}".format(template_filename))
     if os.path.exists(template_filename):
@@ -70,6 +84,17 @@ def create_sample_input_files(template_filename, database_filename):
             'myself@mydomain.com,"Myself",17\n'
             'bob@bobdomain.com,"Bob",42\n'
             )
+    print("Creating sample config file {}".format(config_filename))
+    if os.path.exists(config_filename):
+        print("Error: file exists: " + config_filename)
+        sys.exit(1)
+    with open(config_filename, "w") as config_file:
+        config_file.write(
+            '[smtp_server]\n'
+            'host = smtp.gmail.com\n'
+            'port = 465\n'
+            'username = YOUR_USERNAME_HERE\n'
+            )
     print("Edit these files, and then run mailmerge again")
 
 
@@ -89,12 +114,17 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option("--template", "template_filename",
               default=TEMPLATE_FILENAME_DEFAULT,
               help="template email file name; default " + TEMPLATE_FILENAME_DEFAULT)
+@click.option("--config", "config_filename",
+              default=CONFIG_FILENAME_DEFAULT,
+              help="configuration file name; default " + CONFIG_FILENAME_DEFAULT)
 def main(sample=False,
          dry_run=True,
          limit=1,
          no_limit=False,
          database_filename=DATABASE_FILENAME_DEFAULT,
-         template_filename=TEMPLATE_FILENAME_DEFAULT):
+         template_filename=TEMPLATE_FILENAME_DEFAULT,
+         config_filename=CONFIG_FILENAME_DEFAULT,
+         ):
     #pylint: disable=too-many-arguments
     """mailmerge 0.1 by Andrew DeOrio <awdeorio@umich.edu>
 
@@ -106,7 +136,9 @@ def main(sample=False,
 
     # Create a sample email template and database if there isn't one already
     if sample:
-        create_sample_input_files(template_filename, database_filename)
+        create_sample_input_files(template_filename,
+                                  database_filename,
+                                  config_filename)
         sys.exit(0)
     if not os.path.exists(template_filename):
         print("Error: can't find template email " + template_filename)
@@ -143,19 +175,21 @@ def main(sample=False,
                 if dry_run:
                     print(">>> sent message {} DRY RUN".format(i))
                 else:
-                    sendmail(message)
+                    sendmail(message, config_filename)
                     print(">>> sent message {}".format(i))
 
-                # Hints for user
-                if not no_limit:
-                    print(">>> Limit was {} messages.  ".format(limit) +
-                          "To remove the limit, use the --no-limit option.")
-                if dry_run:
-                    print(">>> This was a dry run.  To send messages, use the --no-dry-run option.")
+            # Hints for user
+            if not no_limit:
+                print(">>> Limit was {} messages.  ".format(limit) +
+                      "To remove the limit, use the --no-limit option.")
+            if dry_run:
+                print(">>> This was a dry run.  To send messages, use the --no-dry-run option.")
 
         except smtplib.SMTPAuthenticationError as e:
             print(">>> Authentication error: {}".format(e))
-
+        except ConfigParser.Error as e:
+            print(">>> Error reading config file {}: {}".format(
+                config_filename, e))
 
 if __name__ == "__main__":
     main()
