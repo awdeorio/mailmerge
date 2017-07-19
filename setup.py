2582a076f049119dbd7d53ae91769cb221abba71
Andrew DeOrio <awdeorio@gmail.com>
2017-07-19 19:13:50 -0400

    bump version to 1.7.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name="mailmerge",
     description="A simple, command line mail merge tool",
     long_description=README,
-    version="1.7.4",
+    version="1.7.5",
     author="Andrew DeOrio",
     author_email="awdeorio@umich.edu",
     url="https://github.com/awdeorio/mailmerge/",
