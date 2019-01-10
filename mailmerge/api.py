bdd6028dffe74d2b421ae233c4e73e45762afed5
Sesh Sadasivam <seshrs@umich.edu>
2019-01-10 09:00:35 -0500

    Supported duplicate attachment headers

--- mailmerge/api.py
+++ mailmerge/api.py
@@ -60,28 +60,35 @@ def parsemail(raw_message):
 
 def addattachments(message, template_path):
     """Add the attachments from the message from the commandline options."""
-    if 'attachments' not in message:
+    if 'attachment' not in message:
         return message, 0
 
     # If the message is not already a multipart message, then make it so
     if not message.is_multipart():
         multipart_message = email.mime.multipart.MIMEMultipart()
-        for header_key in message:
-            multipart_message[header_key] = message[header_key]
+        for header_key in set(message.keys()):
+            # Preserve duplicate headers
+            values = message.get_all(header_key, failobj=[])
+            for value in values:
+                multipart_message[header_key] = value
         original_text = message.get_payload()
         assert isinstance(original_text, str)
         multipart_message.attach(email.mime.text.MIMEText(original_text))
         message = multipart_message
 
-    attachment_filepaths = [filepath.strip()
-                            for filepath in message['attachments'].split(',')
-                            if filepath.strip()]
+    attachment_filepaths = message.get_all('attachment', failobj=[])
     template_parent_dir = os.path.dirname(template_path)
 
     for attachment_filepath in attachment_filepaths:
+        attachment_filepath = attachment_filepath.strip()
+        if not attachment_filepath:
+            continue
+        if not os.path.isabs(attachment_filepath):
+            # Relative paths are relative to the template's parent directory
+            attachment_filepath = os.path.join(template_parent_dir,
+                                               attachment_filepath)
+        normalized_path = os.path.abspath(attachment_filepath)
         # Check that the attachment exists
-        full_path = template_parent_dir + attachment_filepath
-        normalized_path = os.path.abspath(full_path)
         if not os.path.exists(normalized_path):
             print("Error: can't find attachment " + normalized_path)
             sys.exit(1)
