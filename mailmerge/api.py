862b2c66a3b9b5875241a6c2dd27f69fcede61e7
Sesh Sadasivam <seshrs@umich.edu>
2019-04-21 11:48:28 -0400

    Fixed assertion bug Verified that tests pass locally

--- mailmerge/api.py
+++ mailmerge/api.py
@@ -98,7 +98,7 @@ def make_message_multipart(message):
 
 def convert_markdown(message):
     """Convert markdown in message text to HTML."""
-    assert not message['Content-Type'].startswith("text/markdown")
+    assert message['Content-Type'].startswith("text/markdown")
     del message['Content-Type']
     # Convert the text from markdown and then make the message multipart
     message = make_message_multipart(message)
