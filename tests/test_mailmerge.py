4c5ca6457e8180743af32fef0cbab3a062c03d0a
Andrew DeOrio <awdeorio@umich.edu>
2019-10-31 12:07:26 -0400

    First working SMTP test

--- tests/test_mailmerge.py
+++ tests/test_mailmerge.py
@@ -1,8 +1,10 @@
 """Mailmerge unit tests."""
 import os
+import unittest.mock
 import mailmerge
 
 
+# Directories containing test input files
 TEST_DIR = os.path.dirname(__file__)
 TESTDATA_DIR = os.path.join(TEST_DIR, "testdata")
 
@@ -27,3 +29,19 @@ def test_stdout(capsys):
     assert ">>> sent message 0 DRY RUN" in stdout
     assert ">>> message 1" in stdout
     assert ">>> sent message 1 DRY RUN" in stdout
+
+
+@unittest.mock.patch('smtplib.SMTP_SSL')
+def test_smtp(SMTP_SSL):
+    """Verify SMTP library calls."""
+    mailmerge.api.main(
+        database_filename=os.path.join(TESTDATA_DIR, "simple_database.csv"),
+        template_filename=os.path.join(TESTDATA_DIR, "simple_template.txt"),
+        config_filename=os.path.join(TESTDATA_DIR, "server_ssl.conf"),
+        limit=1,
+        dry_run=False,
+    )
+
+    # Mock smtp object with function calls recorded
+    smtp = SMTP_SSL.return_value
+    assert smtp.sendmail.call_count == 1
