8f5e5835d5030eb80b4ef8a2d6a35ecf8ad76002
Andrew DeOrio <awdeorio@umich.edu>
2019-10-31 11:49:28 -0400

    simplify code

--- tests/test_mailmerge.py
+++ tests/test_mailmerge.py
@@ -1,28 +1,10 @@
 """Mailmerge unit tests."""
 import os
-import sys
-import shutil
-import distutils.dir_util
 import mailmerge
 
 
 TEST_DIR = os.path.dirname(__file__)
 TESTDATA_DIR = os.path.join(TEST_DIR, "testdata")
-
-
-def create_and_clean_testdir(dirname):
-    """Create temporary directory for test, if needed."""
-    # Make sure tmp directory exists
-    assert os.path.dirname(dirname) == "tmp"
-    if not os.path.exists("tmp"):
-        os.mkdir("tmp")
-
-    # Remove stale directory
-    if os.path.exists(dirname):
-        shutil.rmtree(dirname)
-
-    # Create directory for this test
-    os.mkdir(dirname)
 
 
 def test_stdout(capsys):
@@ -31,15 +13,12 @@ def test_stdout(capsys):
     pytest docs on capturing stdout and stderr
     https://pytest.readthedocs.io/en/2.7.3/capture.html
     """
-    create_and_clean_testdir("tmp/test_simple")
-    distutils.dir_util.copy_tree(
-        os.path.join(TESTDATA_DIR, "test_simple"),
-        "tmp/test_simple",
+    mailmerge.api.main(
+        database_filename=os.path.join(TESTDATA_DIR, "simple_database.csv"),
+        template_filename=os.path.join(TESTDATA_DIR, "simple_template.txt"),
+        config_filename=os.path.join(TESTDATA_DIR, "simple_server.conf"),
+        no_limit=True,
     )
-
-    # Run mailmerge
-    os.chdir("tmp/test_simple")
-    mailmerge.api.main(no_limit=True)
 
     # Verify mailmerge output
     stdout, stderr = capsys.readouterr()
