e7f780e23457c686699e17594081101b40f079e9
Andrew DeOrio <awdeorio@umich.edu>
2019-10-31 13:02:34 -0400

    Refactor test_attachment

--- tests/test_mailmerge.py
+++ tests/test_mailmerge.py
@@ -2,6 +2,7 @@
 import os
 import unittest.mock
 import email
+import cd
 import markdown
 import mailmerge
 
@@ -96,7 +97,7 @@ def test_markdown(mock_SMTP):
         dry_run=False,
     )
 
-    # Parse sender and recipients from mock calls to sendmail
+    # Parse sender, recipients and message from mock calls to sendmail
     smtp = mock_SMTP.return_value
     assert len(smtp.sendmail.call_args_list) == 1
     sender = smtp.sendmail.call_args_list[0][0][0]
@@ -127,3 +128,60 @@ def test_markdown(mock_SMTP):
     htmltext = payload[1].get_payload()
     rendered = markdown.markdown(plaintext)
     assert f"<html><body>{rendered}</body></html>" == htmltext.strip()
+
+
+@unittest.mock.patch('smtplib.SMTP')
+def test_attachment(mock_SMTP):
+    """Attachments should be sent as part of the email."""
+    with cd.cd(TESTDATA_DIR):
+        mailmerge.api.main(
+            database_filename=os.path.join(TESTDATA_DIR, "simple_database.csv"),
+            template_filename=os.path.join(TESTDATA_DIR, "attachment_template.txt"),
+            config_filename=os.path.join(TESTDATA_DIR, "server_open.conf"),
+            no_limit=False,
+            dry_run=False,
+        )
+
+    # Parse sender, recipients and message from mock calls to sendmail
+    smtp = mock_SMTP.return_value
+    assert len(smtp.sendmail.call_args_list) == 1
+    sender = smtp.sendmail.call_args_list[0][0][0]
+    recipients = smtp.sendmail.call_args_list[0][0][1]
+    raw_message = smtp.sendmail.call_args_list[0][0][2]
+    message = email.parser.Parser().parsestr(raw_message)
+
+    # Verify sender and recipients
+    assert sender == "My Self <myself@mydomain.com>"
+    assert recipients == ["myself@mydomain.com"]
+
+    # Verify message is multipart
+    assert message.is_multipart()
+
+    # Make sure the attachments are all present and valid
+    email_body_present = False
+    expected_attachments = {
+        "attachment_1.txt": False,
+        "attachment_2.pdf": False,
+        "attachment_17.txt": False,
+    }
+    for part in message.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue
+        if part['content-type'].startswith('text/plain'):
+            # This is the email body
+            email_body = part.get_payload()
+            assert email_body.rstrip() == 'Hi, Myself,\n\nYour number is 17.'
+            email_body_present = True
+        elif part['content-type'].startswith('application/octet-stream'):
+            # This is an attachment
+            filename = part.get_param('name')
+            file_contents = part.get_payload(decode=True)
+            assert filename in expected_attachments
+            assert not expected_attachments[filename]
+            filename_testdata = os.path.join(TESTDATA_DIR, filename)
+            with open(filename_testdata, 'rb') as expected_attachment:
+                correct_file_contents = expected_attachment.read()
+            assert file_contents == correct_file_contents
+            expected_attachments[filename] = True
+    assert email_body_present
+    assert False not in expected_attachments.values()
