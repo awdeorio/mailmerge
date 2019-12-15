e11803d76bb88744b87d6d3186d5d87084d22b71
Andrew DeOrio <awdeorio@umich.edu>
2019-12-15 11:18:24 -0500

    add test

--- tests/test_mailmerge.py
+++ tests/test_mailmerge.py
@@ -9,11 +9,7 @@ from . import utils
 
 
 def test_stdout():
-    """Verify stdout and stderr.
-
-    pytest docs on capturing stdout and stderr
-    https://pytest.readthedocs.io/en/2.7.3/capture.html
-    """
+    """Verify stdout and stderr with dry run on simple input files."""
     mailmerge_cmd = sh.Command("mailmerge")
     output = mailmerge_cmd(
         "--template", utils.TESTDATA/"simple_template.txt",
@@ -56,3 +52,17 @@ Your number is 42.
 >>> sent message 1
 >>> This was a dry run.  To send messages, use the --no-dry-run option.
 """
+
+
+def test_no_options():
+    """Verify help message when called with no options."""
+    # FIXME HACK this test assume we're in a folder with no mailmerge_* files
+
+    # Run mailmerge at the CLI with no options.  We expect exit code 1.
+    # https://amoffat.github.io/sh/sections/special_arguments.html#ok-code
+    mailmerge = sh.Command("mailmerge")
+    output = mailmerge(_ok_code=1)
+
+    # Verify output
+    assert "Error: can't find template email mailmerge_template.txt" in output
+    assert "https://github.com/awdeorio/mailmerge" in output
