ea8000ea94dc406f9c6f7edcf806391c0ad523bf
Andrew DeOrio <awdeorio@umich.edu>
2019-12-15 12:05:16 -0500

    add unit test

--- tests/test_mailmerge.py
+++ tests/test_mailmerge.py
@@ -56,3 +56,18 @@ Your number is 42.
 >>> sent message 1
 >>> This was a dry run.  To send messages, use the --no-dry-run option.
 """
+
+
+def test_no_options(tmpdir):
+    """Verify help message when called with no options.
+    Run mailmerge at the CLI with no options.  Do this in an empty temporary
+    directory to ensure that mailmerge doesn't find any default input files.
+    pytest tmpdir docs:
+    http://doc.pytest.org/en/latest/tmpdir.html#the-tmpdir-fixture
+    sh _ok_code docs
+    https://amoffat.github.io/sh/sections/special_arguments.html#ok-code
+    """
+    mailmerge = sh.Command("mailmerge")
+    with tmpdir.as_cwd():
+        output = mailmerge(_ok_code=1)  # expect non-zero exit
+    assert "Error: can't find template email mailmerge_template.txt" in output
