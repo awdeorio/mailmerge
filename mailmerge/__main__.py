06d9fc3cb1ae35bfcf304c0fa3225d861b3e25c3
Andrew DeOrio <awdeorio@umich.edu>
2019-12-18 16:39:31 -0500

    Error output formatting

--- mailmerge/__main__.py
+++ mailmerge/__main__.py
@@ -148,25 +148,24 @@ def check_input_files(template_path, database_path, config_path, sample):
     if not template_path.exists():
         sys.exit(textwrap.dedent(u"""
             Error: can't find template {template_path}
-            Create a sample (--sample) or specify a file (--template)
 
-            See https://github.com/awdeorio/mailmerge for examples.
+            Create a sample (--sample) or specify a file (--template)
+            See https://github.com/awdeorio/mailmerge for examples.\
         """.format(template_path=template_path)))
 
     if not database_path.exists():
         sys.exit(textwrap.dedent(u"""
             Error: can't find database {database_path}
-            Create a sample (--sample) or specify a file (--database)
 
-            See https://github.com/awdeorio/mailmerge for examples.
+            Create a sample (--sample) or specify a file (--database)
+            See https://github.com/awdeorio/mailmerge for examples.\
         """.format(database_path=database_path)))
 
     if not config_path.exists():
         sys.exit(textwrap.dedent(u"""
             Error: can't find config {config_path}
             Create a sample (--sample) or specify a file (--config)
-
-            See https://github.com/awdeorio/mailmerge for examples.
+            See https://github.com/awdeorio/mailmerge for examples.\
         """.format(config_path=config_path)))
 
 
