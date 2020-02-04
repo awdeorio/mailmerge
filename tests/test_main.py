7f90cb74fd76298844fa684e684c170181edd499
Andrew DeOrio <awdeorio@umich.edu>
2020-02-04 18:55:31 -0500

    add unit test

--- tests/test_main.py
+++ tests/test_main.py
@@ -995,3 +995,40 @@ def test_resume_hint_on_csv_error(tmpdir):
     stderr = error.value.stderr.decode("utf-8")
     assert stdout == ""
     assert "--resume 2" in stderr
+
+
+def test_resume_first_message(tmpdir):
+    """Do not print hint about --resume option if it's the first message."""
+    # Simple template with an error
+    template_path = Path(tmpdir/"mailmerge_template.txt")
+    template_path.write_text(textwrap.dedent(u"""\
+        TO: to@test.com
+        FROM: from@test.com
+
+        {{key_not_found}}
+    """))
+
+    # Database with an error
+    database_path = Path(tmpdir/"mailmerge_database.csv")
+    database_path.write_text(textwrap.dedent(u"""\
+        message
+        world
+    """))
+
+    # Simple unsecure server config
+    config_path = Path(tmpdir/"mailmerge_server.conf")
+    config_path.write_text(textwrap.dedent(u"""\
+        [smtp_server]
+        host = open-smtp.example.com
+        port = 25
+    """))
+
+    # Run mailmerge
+    with tmpdir.as_cwd(), pytest.raises(sh.ErrorReturnCode_1) as error:
+        sh.mailmerge()
+
+    # Verify "--resume 1" hint is not in output
+    stdout = error.value.stdout.decode("utf-8")
+    stderr = error.value.stderr.decode("utf-8")
+    assert stdout == ""
+    assert "--resume 1" not in stderr
