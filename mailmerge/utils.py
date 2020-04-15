2a68f1c4d5b0c3431c175bfa27cde81b6394c1df
Andrew DeOrio <awdeorio@umich.edu>
2020-04-15 16:13:27 -0400

    lint

--- mailmerge/utils.py
+++ mailmerge/utils.py
@@ -1,12 +1,24 @@
-# FIXME import style
-from cStringIO import StringIO
-from email.generator import Generator
+"""
+Utility functions used by multiple mailmerge modules.
+
+Andrew DeOrio <awdeorio@umich.edu>
+"""
+import StringIO
+import future.backports.email as email
+import future.backports.email.generator
 
 
 def flatten_message(message):
-    # https://docs.python.org/2/library/email.message.html
-    fp = StringIO()
-    g = Generator(fp, mangle_from_=False, maxheaderlen=78)
-    g.flatten(message)
-    text = fp.getvalue()
+    """Return message as string.
+
+    We can't use Python 3's __str__() because it doesn't work on Python 2.  We
+    can't use message.as_string() because it errors on UTF-8 headers.
+
+    Based on Python 2 documentation
+    https://docs.python.org/2/library/email.message.html
+    """
+    stream = StringIO.StringIO()
+    generator = email.generator.Generator(stream, mangle_from_=False, maxheaderlen=78)
+    generator.flatten(message)
+    text = stream.getvalue()
     return text
