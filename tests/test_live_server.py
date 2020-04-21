8624eea6a230c16a3952deeb767e9b626235e6ac
Andrew DeOrio <awdeorio@umich.edu>
2020-04-21 09:44:47 -0400

    Simplify

--- tests/test_live_server.py
+++ tests/test_live_server.py
@@ -5,7 +5,33 @@ Andrew DeOrio <awdeorio@umich.edu>
 """
 import textwrap
 from pathlib import Path
+import logging
 import sh
+import pytest
+import aiosmtpd.controller
+import aiosmtpd.handlers
+
+
+# The sh library triggers lot of false no-member errors
+# pylint: disable=no-member
+
+# Test fixtures need to be inputs, but aren't always used
+# pylint: disable=unused-argument
+
+logging.getLogger("sh").setLevel(logging.WARNING)
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.fixture(name='live_smtp_server')
+def setup_teardown_live_smtp_server():
+    """Start a message-swallowing SMTP server in a separate thread."""
+    controller = aiosmtpd.controller.Controller(
+        aiosmtpd.handlers.Sink(),
+        port=8025,
+    )
+    controller.start()
+    yield controller
+    controller.stop()
 
 
 def test_simple(tmpdir, live_smtp_server):
